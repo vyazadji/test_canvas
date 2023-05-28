4dcea23177e8638342ebd6ba57dd131d9d803c0a
Vasiliy Yazadji <vyaz@MacBook-Pro-Vasiliy.local>
2023-05-28 11:42:04 +0300

    chore: add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,10 +4,14 @@ module.exports = {
     es2021: true,
   },
   root: true,
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors
+  ],
   rules: {
-    semi: ["warn", "never"],
+    semi: ['warn', 'never'],
   },
 }
