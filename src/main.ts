aeaf2a07b9f6aec55dec3d47ba2e2be7c0d9d819
Vasiliy Yazadji <vyaz@MacBook-Pro-Vasiliy.local>
2023-05-31 21:27:32 +0300

    chore: refactor html position; change favicon

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,11 @@
 import './style.css'
 import app from './app.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+/* document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h1>Test component decomposition</h1>
-    <div class="apps">
-      <div id="app1"></div>
-      <div id="menu" >
-        <button id="addComponent" style="display: none"> Add Component </button>
-      </div>
-    </div>
   </div>
-`
+` */
 
 document.addEventListener('DOMContentLoaded', (_event) => {
   const app1 = new app(document.querySelector<HTMLDivElement>('#app1')!)
