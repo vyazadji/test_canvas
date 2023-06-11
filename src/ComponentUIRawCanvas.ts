bdd1696fb52352e4e1f0df0f04d5ad59d403609a
Vasiliy Yazadji <vyaz@MacBook-Pro-Vasiliy.local>
2023-06-11 21:20:29 +0300

    enh: add different raw canvas elements

--- src/ComponentUIRawCanvas.ts
+++ src/ComponentUIRawCanvas.ts
@@ -2,6 +2,61 @@ import { random } from 'lodash'
 
 import { ComponentUI } from './type'
 import { COMPONENT_HEIGHT, COMPONENT_WIDTH, VIEW_HEIGHT, VIEW_WIDTH } from './consts'
+import { getRandomColor } from './utils/colors'
+
+const figureTemplates = [
+  {
+    // filled rect
+    getFigure: (
+      context: CanvasRenderingContext2D,
+      backgroundColor: string,
+      x: number,
+      y: number,
+      randomX: number,
+      randomY: number
+    ): void => {
+      context.fillStyle = backgroundColor
+      context.fillRect(x, y, randomX, randomY)
+    },
+  },
+  {
+    //Filled triangle
+    getFigure: (
+      context: CanvasRenderingContext2D,
+      backgroundColor: string,
+      x: number,
+      y: number,
+      randomX: number,
+      randomY: number
+    ): void => {
+      const randomX2 = randomX / 2
+      const randomY2 = randomY / 2
+      context.fillStyle = backgroundColor
+      context.beginPath()
+      context.moveTo(x + randomX2, y + randomY2)
+      context.lineTo(x + randomX2 + 20, y + randomY2)
+      context.lineTo(x + randomX2, y + randomY2 + 20)
+      context.fill()
+    },
+  },
+  {
+    //circle
+    getFigure: (
+      context: CanvasRenderingContext2D,
+      _backgroundColor: string,
+      x: number,
+      y: number,
+      randomX: number,
+      randomY: number
+    ): void => {
+      const randomX2 = randomX / 2
+      const randomY2 = randomY / 2
+      context.beginPath()
+      context.arc(x + randomX2, y + randomY2, randomX2, 0, Math.PI * 2, true) // Внешняя окружность
+      context.stroke()
+    },
+  },
+]
 
 /**
  * UI Canvas implementation of component.
@@ -15,26 +70,44 @@ class ComponentUIRawCanvasClass implements ComponentUI {
   directionX: number
   directionY: number
   dataSourceNumber: number
+  backgroundColor: string
+  randomX: number
+  randomY: number
+  figure: number
 
   constructor(context: CanvasRenderingContext2D) {
     this.context = context
     this.dataSourceNumber = 0
+    this.backgroundColor = getRandomColor()
 
     // init position
     this.x = random(VIEW_WIDTH - COMPONENT_WIDTH)
     this.y = random(VIEW_HEIGHT - COMPONENT_HEIGHT)
 
-    //init direction
+    // random position for figure inside component
+    this.randomX = random(COMPONENT_WIDTH)
+    this.randomY = random(COMPONENT_HEIGHT)
+
+    this.figure = random(figureTemplates.length - 1)
+
+    //init direction. need for moving
     this.directionX = 0
     this.directionY = 0
   }
 
   draw(number: number) {
     this.dataSourceNumber = number
+    this.context.clearRect(this.x, this.y, COMPONENT_WIDTH, COMPONENT_HEIGHT)
 
     // Draw yellow background
-    this.context.fillStyle = 'yellow'
-    this.context.fillRect(this.x, this.y, COMPONENT_WIDTH, COMPONENT_HEIGHT)
+    figureTemplates[this.figure].getFigure(
+      this.context,
+      this.backgroundColor,
+      this.x,
+      this.y,
+      this.randomX,
+      this.randomY
+    )
 
     // Draw the number
     this.context.fillStyle = 'black'
