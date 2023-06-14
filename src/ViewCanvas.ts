7274d2e92d1bcfafe2bafdb6f415bdbf9d08d3be
Vasiliy Yazadji <vyaz@MacBook-Pro-Vasiliy.local>
2023-06-14 23:40:33 +0300

    enh: use PostionManager in Canvas View

--- src/ViewCanvas.ts
+++ src/ViewCanvas.ts
@@ -1,7 +1,6 @@
 import type { Component } from './type'
 import ComponentUIRawCanvasClass from './ComponentUIRawCanvas'
-
-import { COMPONENT_HEIGHT, COMPONENT_WIDTH } from './consts'
+import PositionManager from './PositionManager'
 
 /**
  * Canvas layer
@@ -13,6 +12,7 @@ class ViewCanvasDashboard {
   context: CanvasRenderingContext2D
   components: Component[]
   movedComponents: Component[]
+  positionManager: PositionManager
 
   constructor(height: number, width: number) {
     this.components = []
@@ -20,6 +20,10 @@ class ViewCanvasDashboard {
     this.height = height
 
     this.movedComponents = []
+    this.positionManager = new PositionManager({
+      viewWidth: this.width,
+      viewHeight: this.height,
+    })
 
     this.containerEl = document.createElement('canvas')
     this.containerEl.width = this.width
@@ -67,66 +71,26 @@ class ViewCanvasDashboard {
     }
     this.movedComponents.forEach((component) => {
       const el = component.getUIElement() as ComponentUIRawCanvasClass
-      // randomly choose moving directions
-      const x = Math.random() < 0.5 ? -1 : 1
-      const y = Math.random() < 0.5 ? -1 : 1
-      // 0 - not move
-      // -1 move left or top
-      // 1 move right or bottom
-      el.directionX = x // 0 means it will not move left-right
-      el.directionY = y // 0 means not move top-bottom
+
+      this.positionManager.addPosition(el.x, el.y)
     })
 
-    this.moveElement()
+    this.moveElements()
   }
 
-  /**
-   * This function change position of one element
-   * We calculate the next position base on direction x and y
-   */
-  moveElement() {
-    this.movedComponents.forEach((component) => {
-      const el = component.getUIElement() as ComponentUIRawCanvasClass
-      // current position
-      const left = el.x
-      const top = el.y
+  moveElements() {
+    for (let i = 0; i < this.movedComponents.length; i++) {
+      // update position
+      this.positionManager.calculateNextPosition(i)
 
-      // detect direction
+      // set new position
+      const el = this.movedComponents[i].getUIElement() as ComponentUIRawCanvasClass
+      const x = this.positionManager.positions[i].x
+      const y = this.positionManager.positions[i].y
+      el.move(x, y)
+    }
 
-      // X direction
-      if (left > this.width - COMPONENT_WIDTH) {
-        // right border -> move element to the left
-        const new_direction_x = -1
-        el.directionX = new_direction_x
-      } else if (left < 1) {
-        // left border -> move element to the right
-        const new_direction_x = 1
-        el.directionX = new_direction_x
-      }
-
-      const direction_x = el.directionX
-
-      // Y direction
-      if (top > this.height - COMPONENT_HEIGHT) {
-        // bottom border -> move element to the top
-        const new_direction_y = -1
-        el.directionY = new_direction_y
-      } else if (top < 1) {
-        // top border -> move element to the bottom
-        const new_direction_y = 1
-        el.directionY = new_direction_y
-      }
-
-      const direction_y = el.directionY
-
-      const leftNew = left + 1 * direction_x
-      const topNew = top + 1 * direction_y
-
-      // canvas way how to change position
-      el.move(leftNew, topNew)
-    })
-
-    requestAnimationFrame(() => this.moveElement()) // Continue moving element in the next frame
+    requestAnimationFrame(() => this.moveElements()) // Continue moving element in the next frame
   }
 }
 
