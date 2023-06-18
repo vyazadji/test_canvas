6d80882f30f5e49d00a891bc0a1d457f8bdb4e93
Vasiliy Yazadji <vyaz@MacBook-Pro-Vasiliy.local>
2023-06-18 15:02:55 +0300

    enh: save test results in localstorage; test refactoring

--- src/PerformanceTest.ts
+++ src/PerformanceTest.ts
@@ -2,19 +2,38 @@ import FPSMonitor from './FPSMonitor'
 import { addClick } from './utils/helpers'
 import type { App } from './type'
 import type { AppHTML } from './app'
+import Application from './app'
 import type { AppSVG } from './app_svg'
 import type { AppCanvas } from './app_canvas'
 import { VIEW_TYPE } from './consts'
+
+type TestCase = {
+  viewType: string
+  note: string
+  elementsCount: number
+  addComponents: (app: App, componentsCount: number, elementsCount?: number) => void
+}
+
+type TestCaseResult = {
+  viewType: string
+  note: string
+  isDataSource: boolean
+  componentsCount: number
+  elementsCount: number
+  FPS: number
+}
 
 /**
  * This is a test of performance for different technogies
  */
 class PerformanceTest {
-  testCases: object[]
+  testCases: TestCase[]
   testDuration: number // sec
+  localStorageKey: string
 
   constructor() {
     this.testDuration = 2 // sec
+    this.localStorageKey = 'performanceTest'
 
     addClick('startPerformanceTest', () => {
       this.start()
@@ -23,36 +42,37 @@ class PerformanceTest {
     this.testCases = [
       {
         viewType: VIEW_TYPE.HTML,
-        name: 'Html view; html components',
-        test: (app: AppHTML) => {
-          const componentsCount = 5
-          const elementsCount = 2
-          app.addSvgInHtmlComponents(componentsCount, elementsCount)
+        note: 'Html view; html components',
+        elementsCount: 2,
+        addComponents: (app: App, componentsCount: number, elementsCount = 2) => {
+          if (app instanceof Application) {
+            app.addSvgInHtmlComponents(componentsCount, elementsCount)
+          } else {
+            console.warn('Incomplete app type')
+          }
         },
       },
-      {
+      /* {
         viewType: VIEW_TYPE.HTML,
-        name: 'Html view; html components',
-        test: (app: AppHTML) => {
-          const componentsCount = 500
-          const elementsCount = 2
+        note: 'Html view; html components',
+        elementsCount: 2,
+        addComponents: (app: AppHTML, componentsCount: number, elementsCount: number) => {
           app.addSvgInHtmlComponents(componentsCount, elementsCount)
         },
       },
       {
         viewType: VIEW_TYPE.SVG,
-        name: 'SVG view',
-        test: (app: AppSVG) => {
-          const componentsCount = 20
-          const elementsCount = 2
+        note: 'SVG view',
+        elementsCount: 2,
+        addComponents: (app: AppSVG, componentsCount: number, elementsCount: number) => {
           app.addSvgComponentsInSvg(componentsCount, elementsCount)
         },
-      },
+      }, */
       {
         viewType: VIEW_TYPE.CANVAS,
-        name: 'SVG view',
-        test: (app: AppCanvas) => {
-          const componentsCount = 5
+        note: 'Canvas view',
+        elementsCount: 2, // NOTE we can't change. In Canvas we can create component only from 2 elements
+        addComponents: (app: AppCanvas, componentsCount: number, _el: number) => {
           app.addRawCanvasComponents(componentsCount)
         },
       },
@@ -60,8 +80,9 @@ class PerformanceTest {
   }
 
   start() {
+    this.cleanResults()
     //
-    // Go To first HTML view and add "isTest"
+    // Go To HTML view and run first test
     //
     this.navigateToTest(0)
   }
@@ -71,40 +92,51 @@ class PerformanceTest {
 
     const fpsMonitor = new FPSMonitor()
 
-    const currentViewType = this.testCases[testNumber].viewType
-    const currentTestRun = this.testCases[testNumber].test
+    const componentsCount = 50
+    const { viewType, note, elementsCount, addComponents } = this.testCases[testNumber]
 
-    currentTestRun(app)
+    // add elements
+    addComponents(app, componentsCount, elementsCount)
+
+    // start moving
     app.moveTest()
+
+    // calculate FPS
     const avarageFPS = await fpsMonitor.test(this.testDuration)
-    console.log('avarage FPS:', avarageFPS)
+    console.log('Avarage FPS:', avarageFPS)
+
+    // stop Data Source
     app.stopDataSource()
     const noDataSourceavarageFPS = await fpsMonitor.test(this.testDuration)
-    console.log('noDataSource AvarageS FPS:', noDataSourceavarageFPS)
+    console.log('No DataSource Avarages FPS:', noDataSourceavarageFPS)
 
-    console.table([
+    const results: TestCaseResult[] = [
       {
-        view: currentViewType,
-        // components: componentsCount,
-        // elements: elementsCount,
-        dataSource: true,
-        note: 'SVG components',
+        viewType,
+        componentsCount,
+        elementsCount,
+        isDataSource: true,
+        note,
         FPS: avarageFPS,
       },
       {
-        view: currentViewType,
-        // components: componentsCount,
-        // elements: elementsCount,
-        dataSource: false,
-        note: 'SVG components',
+        viewType,
+        componentsCount,
+        elementsCount,
+        isDataSource: false,
+        note,
         FPS: noDataSourceavarageFPS,
       },
-    ])
+    ]
+
+    this.saveResults(results)
 
     // got to next test
     if (this.testCases.length === testNumber + 1) {
       // this the last test
       console.log('!!! THIS is the Testing End !!!')
+      const allResults = this.getResults()
+      console.table(allResults)
     } else {
       // got to the next test
       const nextVewType = this.testCases[testNumber + 1].viewType
@@ -132,6 +164,36 @@ class PerformanceTest {
 
     // Navigate to the new URL with test = 0
     window.location.href = url.toString()
+  }
+
+  //
+  // Save/get results from localStorage
+  //
+
+  cleanResults(): void {
+    // Remove data from localStorage
+    localStorage.removeItem(this.localStorageKey)
+  }
+
+  saveResults(results: TestCaseResult[]): void {
+    // Get data from localStorage
+    let data = this.getResults()
+
+    data = [...data, ...results]
+
+    // save in localStorage
+    localStorage.setItem(this.localStorageKey, JSON.stringify(data))
+  }
+
+  getResults(): TestCaseResult[] {
+    const data: string | null = localStorage.getItem(this.localStorageKey)
+    let results: TestCaseResult[] = data ? JSON.parse(data) : null
+
+    if (!results) {
+      results = []
+    }
+
+    return results
   }
 }
 
