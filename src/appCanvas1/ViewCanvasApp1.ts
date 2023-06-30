7a504f6d6fa4df333b19b7f38003c26f9705cf01
Vasiliy Yazadji <vyaz@MacBook-Pro-Vasiliy.local>
2023-06-30 16:12:57 +0300

    enh: App1 - add one component dragging

--- src/appCanvas1/ViewCanvasApp1.ts
+++ src/appCanvas1/ViewCanvasApp1.ts
@@ -21,6 +21,8 @@ class ViewCanvasApp1 {
   lastPosition: { x: number; y: number }
   needsRedraw: boolean
 
+  draggingComponent: ComponentClass | null
+
   constructor(height: number, width: number) {
     this.width = width
     this.height = height
@@ -33,6 +35,8 @@ class ViewCanvasApp1 {
     this.isPanning = false
     this.lastPosition = { x: 0, y: 0 }
     this.needsRedraw = false
+
+    this.draggingComponent = null
 
     this.canvas = document.createElement('canvas')
     this.canvas.width = this.width
@@ -102,61 +106,94 @@ class ViewCanvasApp1 {
   //
   //
   handleMouseDown = (event: MouseEvent) => {
-    this.isPanning = true
-    this.lastPosition = { x: event.clientX, y: event.clientY }
+    //
+    // Drag full layout
+    //
+    if (event.metaKey) {
+      // handle panning when metaKey is press
+      this.isPanning = true
+      this.lastPosition = { x: event.clientX, y: event.clientY }
+      return
+    }
+
+    //
+    // Drag one component
+    //
+    // Calculate mouse position in canvas
+    const rect = this.canvas.getBoundingClientRect()
+    let mouseX = event.clientX - rect.left
+    let mouseY = event.clientY - rect.top
+
+    // Adjust the position based on the current translation amounts
+    mouseX -= this.offsetX
+    mouseY -= this.offsetY
+
+    // Adjust the position based on the current zoom factor
+    mouseX /= this.zoomFactor
+    mouseY /= this.zoomFactor
+
+    // TODO hardcoded components dimensions
+    const COMPONENT_WIDTH = 10
+    const COMPONENT_HEIGHT = 10
+    // Identify which component was clicked
+    for (const component of this.components) {
+      if (
+        mouseX >= component.x &&
+        mouseX <= component.x + COMPONENT_WIDTH &&
+        mouseY >= component.y &&
+        mouseY <= component.y + COMPONENT_HEIGHT
+      ) {
+        console.log('click on component', component.index, component)
+        this.draggingComponent = component
+        return
+      }
+    }
   }
 
   handleMouseMove = (event: MouseEvent) => {
-    if (!this.isPanning) return
+    //
+    //    Handle panning
+    //
+    if (this.isPanning && event.metaKey) {
+      // Calculate the difference between the current mouse position and the last mouse position.
+      const dx = event.clientX - this.lastPosition.x
+      const dy = event.clientY - this.lastPosition.y
 
-    // Calculate the difference between the current mouse position and the last mouse position.
-    const dx = event.clientX - this.lastPosition.x
-    const dy = event.clientY - this.lastPosition.y
+      // Update the offsets.
+      // this.offsetX += dx / this.zoomFactor
+      // this.offsetY += dy / this.zoomFactor
+      this.offsetX += dx
+      this.offsetY += dy
 
-    // Update the offsets.
-    // this.offsetX += dx / this.zoomFactor
-    // this.offsetY += dy / this.zoomFactor
-    this.offsetX += dx
-    this.offsetY += dy
+      // Update the last position.
+      this.lastPosition = { x: event.clientX, y: event.clientY }
 
-    // Update the last position.
-    this.lastPosition = { x: event.clientX, y: event.clientY }
+      // Redraw the scene.
+      // this.draw()
+      // Flag that a redraw is needed
+      this.needsRedraw = true
+    }
 
-    // Redraw the scene.
-    // this.draw()
-    // Flag that a redraw is needed
-    this.needsRedraw = true
+    //
+    // Handle one component dragging
+    //
+    if (this.draggingComponent) {
+      const rect = this.canvas.getBoundingClientRect()
+      const mouseX = event.clientX - rect.left
+      const mouseY = event.clientY - rect.top
+
+      // Update the position of the dragging component
+      const x = (mouseX - this.offsetX) / this.zoomFactor
+      const y = (mouseY - this.offsetY) / this.zoomFactor
+      this.draggingComponent.position(x, y)
+      this.needsRedraw = true
+    }
   }
 
   handleMouseUp = (_event: MouseEvent) => {
     this.isPanning = false
+    this.draggingComponent = null
   }
-
-  /* handleWheel = (event: WheelEvent) => {
-    event.preventDefault()
-
-    const scaleAmount = 1.1
-
-    // Calculate mouseX and mouseY relative to the canvas, not the window.
-    const rect = this.canvas.getBoundingClientRect()
-    const mouseX = event.clientX - rect.left
-    const mouseY = event.clientY - rect.top
-
-    if (event.deltaY < 0) {
-      // zoom in
-      this.zoomFactor *= scaleAmount
-      this.offsetX -= ((scaleAmount - 1) * (mouseX - this.offsetX)) / this.zoomFactor
-      this.offsetY -= ((scaleAmount - 1) * (mouseY - this.offsetY)) / this.zoomFactor
-    } else {
-      // zoom out
-      this.zoomFactor /= scaleAmount
-      this.offsetX += ((1 - 1 / scaleAmount) * (mouseX - this.offsetX)) / this.zoomFactor
-      this.offsetY += ((1 - 1 / scaleAmount) * (mouseY - this.offsetY)) / this.zoomFactor
-    }
-
-    // Flag that a redraw is needed
-    this.needsRedraw = true
-  } */
 
   handleWheel = (event: WheelEvent) => {
     event.preventDefault()
